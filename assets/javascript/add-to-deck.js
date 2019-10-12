046d3429c06c90af135cb99dd3afc165c14cc5b5
Ray He <rayhe921@gmail.com>
2019-10-12 14:02:28 -0700

    working on displaying html for deck items

--- assets/javascript/add-to-deck.js
+++ assets/javascript/add-to-deck.js
@@ -5,11 +5,12 @@ function deckButton() {
     addButton.addClass("btn btn-primary deck-item inline");
     addButton.text("New Button");
     $("#deck-item-drop-div").append(addButton);
-    console.log(sessionStorage.getItem("spoonId"));
+    console.log(sessionStorage.getItem("theId"));
 };
 
 function buttonDisplay(){
-$(".main-content").text(sessionStorage.getItem("spoonId"));
+$(".ing-and-buttons").text(sessionStorage.getItem("theId"));
+$(".recipe-instructions")
 };
 
     // <button type="button" class="btn btn-primary deck-item inline" draggable="true" ondragstart="drag(event)"
