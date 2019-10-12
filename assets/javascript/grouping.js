57e3cbf909647907d6188b958ffbb9ccb55209f4
kima111 <kima111@gmail.com>
2019-10-11 22:49:04 -0700

    organized index layout, added creation of groups, and added a drag n drop feature

--- assets/javascript/grouping.js
+++ assets/javascript/grouping.js
@@ -1,22 +1,24 @@
 
-    //  var config = {
-    //   apiKey: "AIzaSyBOeyfolSEBghtCgX8D6xgrq3Gj8J4wSyo",
-    //   authDomain: "grouping-persistent-data.firebaseapp.com",
-    //   databaseURL: "https://grouping-persistent-data.firebaseio.com",
-    //   projectId: "grouping-persistent-data",
-    //   storageBucket: "grouping-persistent-data.appspot.com",
-    //   messagingSenderId: "702782983306",
-    //   appId: "1:702782983306:web:48eac313f0274bb2ac7173"
-    // };
+     var config = {
+      apiKey: "AIzaSyBOeyfolSEBghtCgX8D6xgrq3Gj8J4wSyo",
+      authDomain: "grouping-persistent-data.firebaseapp.com",
+      databaseURL: "https://grouping-persistent-data.firebaseio.com",
+      projectId: "grouping-persistent-data",
+      storageBucket: "grouping-persistent-data.appspot.com",
+      messagingSenderId: "702782983306",
+      appId: "1:702782983306:web:48eac313f0274bb2ac7173"
+    };
     
-    // firebase.initializeApp(config);
+    firebase.initializeApp(config);
 
-    // var database = firebase.database();
+    var database = firebase.database();
 
 
 $("#create-group-button").on("click", function(){
     var groupItem = $("#create-group-textBox").val().trim();
-    $("#group-items-list").prepend("<li>" + groupItem + "</li>")
+    var groupItemDiv = "<div id ='group-item' ondrop='drop(event)' ondragover='allowDrop(event)'><button>"
+    var groupItemClosingDiv = "<br><br></button></div><br>"
+    $("#group-items-list").prepend(groupItemDiv + groupItem + groupItemClosingDiv);
 })
 
 
@@ -32,6 +34,11 @@ function allowDrop(ev) {
   function drop(ev) {
     ev.preventDefault();
     var data = ev.dataTransfer.getData("text");
-    console.log(document.getElementById(data));
+    var documentData = document.getElementById(data);
+    
+    database.ref().update({
+    data: documentData
+    })
+    console.log(documentData);
     ev.target.appendChild(document.getElementById(data));
   }
