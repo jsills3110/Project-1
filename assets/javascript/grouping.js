4defe9ece644f4b8527758256ba35f358471f7d1
kima111 <kima111@gmail.com>
2019-10-12 12:41:04 -0700

    updated dropdown list

--- assets/javascript/grouping.js
+++ assets/javascript/grouping.js
@@ -1,28 +1,29 @@
 
-  var config = {
-    apiKey: "AIzaSyBOeyfolSEBghtCgX8D6xgrq3Gj8J4wSyo",
-    authDomain: "grouping-persistent-data.firebaseapp.com",
-    databaseURL: "https://grouping-persistent-data.firebaseio.com",
-    projectId: "grouping-persistent-data",
-    storageBucket: "grouping-persistent-data.appspot.com",
-    messagingSenderId: "702782983306",
-    appId: "1:702782983306:web:48eac313f0274bb2ac7173"
-  };
+var config = {
+  apiKey: "AIzaSyBOeyfolSEBghtCgX8D6xgrq3Gj8J4wSyo",
+  authDomain: "grouping-persistent-data.firebaseapp.com",
+  databaseURL: "https://grouping-persistent-data.firebaseio.com",
+  projectId: "grouping-persistent-data",
+  storageBucket: "grouping-persistent-data.appspot.com",
+  messagingSenderId: "702782983306",
+  appId: "1:702782983306:web:48eac313f0274bb2ac7173"
+};
     
-    firebase.initializeApp(config);
+firebase.initializeApp(config);
+var database = firebase.database();
 
-    var database = firebase.database();
+var groupList = [];
 
 
 $("#create-group-button").on("click", function(){
     var groupItem = $("#create-group-textBox").val().trim();
     var groupItemDiv = "<div id ='group-item' ondrop='drop(event)' ondragover='allowDrop(event)'><button>"
-    var groupItemClosingDiv = "<br><br></button></div><br>"
+    var groupItemClosingDiv = "</button></div><br>"
+    groupList.push(groupItem);
     $("#group-items-list").prepend(groupItemDiv + groupItem + groupItemClosingDiv);
+    createList();
+    console.log(groupList);
 })
-
-
-
 function allowDrop(ev) {
     ev.preventDefault();
   }
@@ -35,10 +36,43 @@ function allowDrop(ev) {
     ev.preventDefault();
     var data = ev.dataTransfer.getData("text");
     var documentData = document.getElementById(data);
-    
+    var stringVer = documentData.outerHTML;
+    debugger;
     database.ref().update({
-    data: documentData
+    data: stringVer
     })
     console.log(documentData);
+    console.log(stringVer);
     ev.target.appendChild(document.getElementById(data));
   }
+  function createListButton(){
+   var dropDownGroupButtonOpenDiv = "<button class='btn btn-secondary dropdown-toggle recipe-group-button' type='button' id='dropdownMenuButton' data-toggle='dropdown' aria-haspopup='true' aria-expanded='false'>";
+   var buttonDropDownName = "Add Recipe to Group";
+   var dropDownGroupButtonCloseDiv = "</button>";
+   var dropDownListOpenDiv = "<div id = 'groupList' class='dropdown-menu' aria-labelledby='dropdownMenuButton'>";
+   var dropDownListCloseDiv = "</div>"
+   $("#listButton").append(dropDownGroupButtonOpenDiv + buttonDropDownName + dropDownGroupButtonCloseDiv + dropDownListOpenDiv + dropDownListCloseDiv);
+   
+  }
+ createListButton();
+  function createList(){
+    
+      var listOpenDiv = "<a class='dropdown-item' href='#'>"
+      var listText = groupList[groupList.length - 1];
+      var listCloseDiv = "</a>";
+      $("#groupList").append(listOpenDiv + listText + listCloseDiv);
+    
+  }
+  $(document).on("click", ".recipe-group-button", function(){
+    console.log("OH_YEAH!")
+  })
+//   <div class="dropdown">
+//   <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+//     Dropdown button
+//   </button>
+//   <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+//     <a class="dropdown-item" href="#">Action</a>
+//     <a class="dropdown-item" href="#">Another action</a>
+//     <a class="dropdown-item" href="#">Something else here</a>
+//   </div>
+// </div>
