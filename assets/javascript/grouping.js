2f9e7f792646fe9cd96c90b9654000c3c03cbaa3
kima111 <kima111@gmail.com>
2019-10-14 13:49:03 -0700

    updated grouping functionality

--- assets/javascript/grouping.js
+++ assets/javascript/grouping.js
@@ -1,29 +1,28 @@
-var config = {
-  apiKey: "AIzaSyBOeyfolSEBghtCgX8D6xgrq3Gj8J4wSyo",
-  authDomain: "grouping-persistent-data.firebaseapp.com",
-  databaseURL: "https://grouping-persistent-data.firebaseio.com",
-  projectId: "grouping-persistent-data",
-  storageBucket: "grouping-persistent-data.appspot.com",
-  messagingSenderId: "702782983306",
-  appId: "1:702782983306:web:48eac313f0274bb2ac7173"
-};
-    
-firebase.initializeApp(config);
-var database = firebase.database();
 var groupList = [];
 var groupOfRecipies = [];
+var groupOfRecipieTitle = [];
 var currentRecipeID;
+var currentRecipeTitle;
+var groupItem;
+
+
+var groupingsOfRecipies = {};
 
 //creating groups functionality
 //this is what happens when you click on create group button
 $("#create-group-button").on("click", function(){
-    var groupItem = $("#create-group-textBox").val().trim();
+    groupItem = $("#create-group-textBox").val().trim();
     // var groupItemDiv = "<div id ='group-item' ondrop='drop(event)' ondragover='allowDrop(event)'><button>"
     var groupItemDiv = "<div class = 'group-item'><button class = 'group-item-button'>"
     var groupItemClosingDiv = "</button></div><br>"
+
+
+    groupingsOfRecipies[groupItem] = [];
+    console.log(groupingsOfRecipies);
     groupList.push(groupItem);
-    $("#group-items-list").prepend(groupItemDiv + groupItem + groupItemClosingDiv);
-    createList();
+    localStorage.setItem("groupList", groupList);
+    $("#group-items-list").append(groupItemDiv + groupItem + groupItemClosingDiv);
+   createList();
 })
 
 //creates list button; which lists all the groups
@@ -40,46 +39,59 @@ function createListButton(){
 }
 //creates the lists of groups
 function createList(){
+  $("#groupList").empty();
+  for (i=0;i<groupList.length; i++){
     var listOpenDiv = "<a class='dropdown-item group-list-button' href='#'>"
-    var listText = groupList[groupList.length - 1];
+    var listText = groupList[i];
     var listCloseDiv = "</a>";
     $("#groupList").append(listOpenDiv + listText + listCloseDiv);
+  }
+  
 }
 
-$(document).on("click", ".recipe-group-button", function(){
-    currentRecipeID = $("#add-to-deck").attr("recipe-id");
-    groupOfRecipies.push(currentRecipeID);
-    console.log(groupOfRecipies);
+$(document).on("click", ".group-list-button", function(){
+  currentRecipeID = $("#add-to-deck").attr("recipe-id");
+  groupOfRecipies.push(currentRecipeID);
+  tempJSON = JSON.parse(sessionStorage.getItem(currentRecipeID));
+  currentRecipeTitle = tempJSON.title;
+  groupOfRecipieTitle.push(currentRecipeTitle);
+
+  for(i=0;i<groupList.length;i++){
+    if(groupList[i] == this.text){
+      groupingsOfRecipies[this.text].push(currentRecipeID);
+    }
+  }
 
 })
-$(document).on("click",".group-list-button", function(){
 
-
-  //   function deckButton() {
-//     var addButton = $("<button>");
-//     addButton.attr("type", "button");
-//     addButton.addClass("btn btn-primary deck-item inline");
-//     addButton.text("New Button");
-//     $("#deck-item-drop-div").append(addButton);
-//     id = $(this).attr("recipe-id");
-//     ingredients = $(this).attr("recipe-id");
-// };
-
-})
 $(document).on("click", ".group-item-button", function(){
-  displayRecipe();
+  $("#deck-item-drop-div").empty();
+  var buttonText = $(this).text();
+   for(i=0; i<groupingsOfRecipies[buttonText].length; i++){
+    currentRecipeTitle = JSON.parse(sessionStorage.getItem(groupingsOfRecipies[buttonText][i])).title;
+    var tempRecipeID = groupingsOfRecipies[buttonText][i];
+    var deckButtonDiv = $("<button>");
+    deckButtonDiv.attr("type", "button");
+    deckButtonDiv.addClass("btn btn-primary deck-item set-id inline");
+    deckButtonDiv.attr("recipe-id", tempRecipeID);
+    deckButtonDiv.text(currentRecipeTitle);
+    $("#deck-item-drop-div").append(deckButtonDiv)
+  }
 
 })
-function groupToDeck(){
-  var deckButtonDiv = $("<button>");
-  deckButtonDiv.attr("type", "button");
-  deckButtonDiv.addClass("btn btn-primary deck-item inline");
-  deckButtonDiv.text("Group_Deck_Button");
-  $("#deck-item-drop-div").append(deckButtonDiv)
-}
+
+$(document).on("click", ".group-deck-item", function(){
+  displayRecipe();
+})
+$(document).on("click", ".set-id", function(){
+    id = $(this).attr("recipe-id");
+})
 
 
-//drag and drop functionality
+
+
+
+//drag and drop functionality - future feature
 function allowDrop(ev) {
     ev.preventDefault();
 }
